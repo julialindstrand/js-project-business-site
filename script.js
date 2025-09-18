c91e5888023858d95311dcad504b132ef905af53
Julia <juliakeo@pm.me>
2025-09-18 14:30:06 +0200

    Clean the code

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function myFunction(meny){
-  meny.classList.toggle("change");
+  meny.classList.toggle("change")
   document.querySelector(".navbar ul").classList.toggle("change")
 }
+
+// function myFunction() {
+//    const body = document.body
+//    body.classList.toggle("dark-mode-body")
+//   }
